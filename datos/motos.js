b8f1e068ae4515b9a57beba65e1df015592025ef
Magss28 <magaoli14@gmail.com>
2026-06-01 18:40:25 -0300

    motos

--- datos/motos.js
+++ datos/motos.js
@@ -14,6 +14,230 @@ export const motos = [
     precioConce: 75000,
     categoria: "Motos",
     activo: true
+  },
+
+  {
+    marca: "Shitzu",
+    modelo: "Defiler",
+    precioConce: 70000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Dinka",
+    modelo: "Vindicator",
+    precioConce: 55000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Principe",
+    modelo: "Diabolus",
+    precioConce: 60000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Shitzu",
+    modelo: "Vader",
+    precioConce: 85000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Nagasaki",
+    modelo: "Carbon RS",
+    precioConce: 150000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Shitzu",
+    modelo: "Hakuchou",
+    precioConce: 125000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "LCC",
+    modelo: "Innovation",
+    precioConce: 70000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Pegassi",
+    modelo: "Bati 801",
+    precioConce: 135000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Western",
+    modelo: "Wolfsbane",
+    precioConce: 70000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Western",
+    modelo: "Cliffhanger",
+    precioConce: 60000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Western",
+    modelo: "Gargoyle",
+    precioConce: 90000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Pegassi",
+    modelo: "Faggio Sport",
+    precioConce: 10000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Western",
+    modelo: "Zombie Bobber",
+    precioConce: 75000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Pegassi",
+    modelo: "FCR 1000",
+    precioConce: 75000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Shitzu",
+    modelo: "PCJ-600",
+    precioConce: 65000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Principe",
+    modelo: "Lectro",
+    precioConce: 85000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Western",
+    modelo: "Zombie Chopper",
+    precioConce: 75000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "LCC",
+    modelo: "Avarus",
+    precioConce: 80000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Pegassi",
+    modelo: "Vortex",
+    precioConce: 90000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Dinka",
+    modelo: "Enduro",
+    precioConce: 30000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Western",
+    modelo: "Reever",
+    precioConce: 140000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Dinka",
+    modelo: "Double-T",
+    precioConce: 160000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Pegassi",
+    modelo: "Ruffian",
+    precioConce: 70000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Maibatsu",
+    modelo: "Sanchez",
+    precioConce: 85000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "WMC",
+    modelo: "Bagger",
+    precioConce: 60000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Nagasaki",
+    modelo: "Shinobi",
+    precioConce: 230000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Dinka",
+    modelo: "Akuma",
+    precioConce: 120000,
+    categoria: "Motos",
+    activo: true
+  },
+
+  {
+    marca: "Dinka",
+    modelo: "Thrust",
+    precioConce: 70000,
+    categoria: "Motos",
+    activo: true
   }
 
 ];
